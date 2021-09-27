eaafe133c16612095deefda7d8c45b7f2e0b5f6e
showmik <77013640+Miskat-UL@users.noreply.github.com>
2021-09-27 14:10:29 +0600

    uP

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customers/', views.customers),
+    path('', views.home, name='home'),
+    path('products/', views.products, name='product'),
+    path('customers/<str:pk>', views.customers, name='customers'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render
-
+from .models import Product, Order, Tag, Customer
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    customer = Customer.objects.all()
+    order = Order.objects.all()
+    total_customers = customer.count()
+    total_order = order.count()
+    delivered = order.filter(status="Delivered").count()
+    pending = order.filter(status="pending").count()
+    context = {
+        'customers': customer,
+        'orders': order,
+        'total_order': total_order,
+        'delivered': delivered,
+        'pending': pending
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'accounts/products.html', context)
 
 
-def customers(request):
-    return render(request, 'accounts/customer.html')
+def customers(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    context = {
+        'customers': customer,
+        'orders': orders
+    }
+    return render(request, 'accounts/customer.html', context)
 
